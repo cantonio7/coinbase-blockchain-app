11a435e0938ef08030fd1743dba6038bb8d0765d
cantonio7 <caioantonio@montsaintcapital.com>
2022-04-19 18:05:30 -0300

    frist commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@thirdweb-dev/sdk'
+
+const supportedChains = [4, 80001]
+const connectors = {
+  injected: {},
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
