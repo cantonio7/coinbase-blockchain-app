178aa48caeb8afac362fc55b9d6d02576e40c748
cantonio7 <caioantonio@montsaintcapital.com>
2022-04-22 15:26:59 -0300

    .env out

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import '../styles/globals.css'
-import { ThirdwebWeb3Provider } from '@thirdweb-dev/sdk'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
 
-const supportedChains = [4, 80001]
+const supportedChains = [4]
 const connectors = {
   injected: {},
 }
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider
+      supportedChains={supportedChains}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp
